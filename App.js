a8190cbbe1d850499f802f07eeffa6959f3ac3a8
Abir4183 <podderjosim@gmail.com>
2025-06-03 06:56:39 +0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Skill Rewards Platform</h1>
+      <p>Welcome to the skill-based gaming and rewards system.</p>
+    </div>
+  );
+}
